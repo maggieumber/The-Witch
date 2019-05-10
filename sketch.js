764fd5ea7c8935265ba9387ecdf1ce903aeb28e8
maggieumber <maggieumber@gmail.com>
2019-05-10 13:39:49 -0500

    added animation and code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var TwirlDistort;
+
+function preload() {
+
+	TwirlDistort = loadAnimation('assets/twistDistort_0000.png', 'assets/twistDistort_0005.png');
+	//by default animations loop but it can be changed
+}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
+	animation(TwirlDistort, windowWidth, windowHeight);
 }
